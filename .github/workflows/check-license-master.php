00f97fcc20525e05afc92bae8927fb7ca93c2848
jdarwood007 <unmonitored+github@sleepycode.com>
2020-12-06 16:29:16 -0800

    Lets keep trying till it works..

---
+++ .github/workflows/check-license-master.php
@@ -0,0 +1,33 @@
+<?php
+// Stuff we will ignore.
+$ignoreFiles = array(
+	'\.github/',
+	'\.buildTools/',
+);
+
+$curDir = '.';
+if (isset($_SERVER['argv'], $_SERVER['argv'][1]))
+	$curDir = $_SERVER['argv'][1];
+
+$foundBad = false;
+foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($curDir, FilesystemIterator::UNIX_PATHS)) as $currentFile => $fileInfo)
+{
+	// Only check PHP
+	if ($fileInfo->getExtension() !== 'php')
+		continue;
+
+	foreach ($ignoreFiles as $if)
+		if (preg_match('~' . $if . '~i', $currentFile))
+			continue 2;
+
+	$result = trim(shell_exec('php buildTools/check-license.php ' . $currentFile));
+
+	if (!preg_match('~Error:([^$]+)~', $result))
+		continue;
+
+	$foundBad = true;
+	fwrite(STDERR, $result);
+}
+
+var_dump($foundBad);
+exit($foundBad ? 1 : 0);
